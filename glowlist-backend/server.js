b2cbf0245bdd92c1278f47a35b85ccc5dedc79ca
tugaszalfaakun <tugaszalfaakun@gmail.com>
2026-08-11 15:05:32 +0700

    membuat web program skincare

---
+++ glowlist-backend/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'glowlist_db'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Gagal konek ke database:', err);
+    } else {
+        console.log('Berhasil konek ke database GlowList');
+    }
+});
+
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Selamat Datang Di GlowList API')
+});
+
+app.get('/produk', (req, res) => {
+    const sql = 'SELECT * FROM produk';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
+
+app.get('/kategori', (req, res) => {
+    const sql = 'SELECT * FROM kategori';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server GlowList jalan di http://localhost:${PORT}`);
+});
---
+++ glowlist-frontend/src/pages/Tentang.jsx
@@ -0,0 +1,3 @@
+export default function Tentang() {
+    return <h2>Perkenalkan nama saya Zalfa Aurelia,kelas XII RPL 2,Alasan saya suka skincare karena bisa merawat diri dan mempersehat kulit</h2>;
+}
