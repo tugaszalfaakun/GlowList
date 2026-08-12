8ed6e0f88850bd75342db9a80ec652e741474575
tugaszalfaakun <tugaszalfaakun@gmail.com>
2026-08-12 19:37:04 +0700

    response PUT dan DELETE

--- glowlist-backend/server.js
+++ glowlist-backend/server.js
@@ -61,7 +61,18 @@ app.put('/produk/:id_produk', (req, res) => {
     const sql = 'UPDATE produk SET judul=?, deskripsi=?, harga=?, id_kategori=? WHERE id_produk=?';
     db.query(sql, [judul, deskripsi, harga, id_kategori, id_produk], (err, result) => {
         if(err) return res.status(500).json({ error: err.sqlMessage });
+        if (result.affectedRows === 0) return res.status(404).json({ message: 'Produk tidak ditemukan'});
         res.json({ message: 'Produk berhasil diupdate'});
+    });
+});
+
+app.delete('/produk/:id_produk', (req,res) => {
+    const { id_produk } = req.params;
+    const sql = 'DELETE FROM produk WHERE id_produk = ?';
+    db.query(sql, [id_produk], (err, result) => {
+        if (err) return res.status(500).json({ error: err.sqlMessage });
+        if (result.affectedRows === 0) return res.status(404).json({ message: 'Produk tidak ditemukan'});
+        res.json({ message: 'Produk berhasil dihapus!' });
     });
 });
 
